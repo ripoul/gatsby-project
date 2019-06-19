6408baa796f647cec558f746459c5d6888e386d4
Jules LE BRIS <jls.lebris@gmail.com>
2019-06-19 11:29:06 +0200

    add js lint and init travis

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+function IndexPage () {
+  return (
+    <div>Hello world!</div>
+  )
+}
+
+export default IndexPage
